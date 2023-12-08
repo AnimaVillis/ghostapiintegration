b262a88b0184aaa10e3275e53c937d4c51af2e84
AnimaVillis <92624844+AnimaVillis@users.noreply.github.com>
2023-12-08 18:25:36 +0100

    Added options.

--- extend.php
+++ extend.php
@@ -1,9 +1,19 @@
 <?php
 use Flarum\Extend;
 use Flarum\Tags\Tag;
+use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/admin/admin.js')
         ->css(__DIR__.'/resources/less/admin.less'),
+
+    (new Extend\Settings())
+        ->serializeToForum('ghostApiUrl', 'animavillis-ghostapiintegration.api_url'),
+    
+    (new Extend\Settings())
+        ->serializeToForum('ghostApiPath', 'animavillis-ghostapiintegration.api_path'),
+
+    (new Extend\Locales(__DIR__.'/locale')),
+
 ];
---
+++ locale/locale.php
@@ -0,0 +1,6 @@
+﻿<?php
+
+return [
+    'animavillis-ghostapiintegration.api_url' => 'Adres Ghost CMS',
+    'animavillis-ghostapiintegration.api_path' => 'Ścieżka API Ghost',
+];
