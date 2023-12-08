ade4bce9713c175aced78ae9c42b81fb52d38df9
AnimaVillis <92624844+AnimaVillis@users.noreply.github.com>
2023-12-08 17:52:07 +0100

    modified

--- src/extend.php
+++ src/extend.php
@@ -1,12 +1,9 @@
 ﻿<?php
 
-namespace Animavillis\GhostApiIntegration;
-
 use Flarum\Extend;
 
 return [
-    // Inne extender-y...
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
-    // Możliwe inne extender-y...
+    // Inne extender-y...
 ];
