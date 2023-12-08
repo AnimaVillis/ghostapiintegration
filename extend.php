ee65601447fd14fc7674a2de343400189f56ee51
AnimaVillis <92624844+AnimaVillis@users.noreply.github.com>
2023-12-08 18:10:37 +0100

    New

--- extend.php
+++ extend.php
@@ -1,24 +1,9 @@
 <?php
-
-use Flarum\Event\ConfigureWebApp;
-use Flarum\Api\Controller\ListPostsController;
-use Flarum\Api\Controller\ListUsersController;
-use Flarum\Api\Controller\ShowDiscussionController;
-use Flarum\Api\Controller\UpdateDiscussionController;
-use Flarum\Api\Serializer;
-use Flarum\Discussion\Discussion;
-use Flarum\Discussion\Event\Saving as DiscussionSaving;
-use Flarum\Discussion\Filter\DiscussionFilterer;
-use Flarum\Discussion\Search\DiscussionSearcher;
 use Flarum\Extend;
-use Flarum\Post\Post;
-use Flarum\Settings\Event\Saving as SettingsSaving;
-use Flarum\Tags\Api\Serializer\TagSerializer;
 use Flarum\Tags\Tag;
-use Flarum\User\User;
 
 return [
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/forum/admin.js')
+        ->js(__DIR__.'/js/admin/admin.js')
         ->css(__DIR__.'/resources/less/admin.less'),
 ];
