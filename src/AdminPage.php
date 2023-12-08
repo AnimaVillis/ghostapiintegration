27d85fbb6ceee335d92b771f46f7398948aa10a0
AnimaVillis <92624844+AnimaVillis@users.noreply.github.com>
2023-12-08 18:45:20 +0100

    Admin

---
+++ src/AdminPage.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+namespace GhostApiIntegration;
+
+use Flarum\Extend\Frontend;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Flarum\Frontend\Document;
+
+class AdminPage
+{
+    public function __invoke(Document $document)
+    {
+        // Tutaj dodaj logikę obsługi widoku panelu administracyjnego
+    }
+}
