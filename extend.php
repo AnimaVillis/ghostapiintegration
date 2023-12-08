b01dee3f34d0712a2b34270a5cc392b1f6740d8f
AnimaVillis <92624844+AnimaVillis@users.noreply.github.com>
2023-12-08 18:30:54 +0100

    Admin view

--- extend.php
+++ extend.php
@@ -6,7 +6,8 @@ use Illuminate\Contracts\Events\Dispatcher;
 return [
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/admin/admin.js')
-        ->css(__DIR__.'/resources/less/admin.less'),
+        ->css(__DIR__.'/resources/less/admin.less')
+        ->view('custom-settings', 'resources\views\admin\extensions\ghostapiintegration.blade.php'),
 
     (new Extend\Settings())
         ->serializeToForum('ghostApiUrl', 'animavillis-ghostapiintegration.api_url'),
--- resources/views/admin/extensions/ghostapiintegration.blade.php
+++ resources/views/admin/extensions/ghostapiintegration.blade.php
@@ -1,3 +1,26 @@
+@extends('flarum::frontend.content.admin')
 
-<h2>Ghost API Integration Settings</h2>
-<p>This is the admin settings page for Ghost API Integration.</p>
+@section('content')
+    <div class="container">
+        <form>
+            <fieldset>
+                <legend>Ghost CMS Settings</legend>
+                
+                <!-- Ghost CMS Address Field -->
+                <div class="form-group">
+                    <label for="ghost_address">Ghost CMS Address</label>
+                    <input type="text" class="form-control" id="ghost_address" placeholder="Enter Ghost CMS Address">
+                </div>
+
+                <!-- Ghost API Path Field -->
+                <div class="form-group">
+                    <label for="api_path">Ghost API Path</label>
+                    <input type="text" class="form-control" id="api_path" placeholder="Enter Ghost API Path">
+                </div>
+
+                <!-- Save Button -->
+                <button type="submit" class="btn btn-primary">Save Settings</button>
+            </fieldset>
+        </form>
+    </div>
+@endsection
