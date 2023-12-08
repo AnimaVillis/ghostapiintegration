73ca81be245c5cbb46164ea6a053b3dbdfcbf125
AnimaVillis <92624844+AnimaVillis@users.noreply.github.com>
2023-12-08 18:47:18 +0100

    admin

--- extend.php
+++ extend.php
@@ -6,11 +6,9 @@ use Illuminate\Contracts\Events\Dispatcher;
 return [
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/admin/admin.js')
-        ->css(__DIR__.'/resources/less/admin.less'),
-        
-    (new Extend\Frontend('admin'))
+        ->css(__DIR__.'/resources/less/admin.less')
         ->content(GhostApiIntegration\AdminPage::class),
-
+        
     (new Extend\Settings())
         ->serializeToForum('ghostApiUrl', 'animavillis-ghostapiintegration.api_url'),
     
